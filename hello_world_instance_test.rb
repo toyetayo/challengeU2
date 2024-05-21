4acf41c692bc0a14f895cb57d1d8b4b1e443435f
Timothy Oyetayo <toyetayo@academic.rrc.ca>
2024-05-21 10:31:48 -0500

    updated hello_world_instance_test.rb

--- hello_world_instance_test.rb
+++ hello_world_instance_test.rb
@@ -15,35 +15,25 @@ begin
   class HelloWorldTest < Minitest::Test
     def test_no_method_name
       
-      result = HelloWorld.new('Wally').hello
-      puts result
       assert_equal 'Hello, World. My name is Wally!', HelloWorld.new('Wally').hello
     end
   
     def test_no_method_name_different_object_name
       
-      result = HelloWorld.new('Daisy').hello
-      puts result
       assert_equal 'Hello, World. My name is Daisy!', HelloWorld.new('Daisy').hello
     end
   
     def test_sample_name
       
-      result = HelloWorld.new('Wally').hello('Alice')
-      puts result
       assert_equal 'Hello, Alice. My name is Wally!', HelloWorld.new('Wally').hello('Alice')
     end
   
     def test_other_sample_name
-      
-      result = HelloWorld.new('Wally').hello('Bob')
-      puts result
       assert_equal 'Hello, Bob. My name is Wally!', HelloWorld.new('Wally').hello('Bob')
     end
   
     def test_other_sample_name_with_different_object_name
-      result = HelloWorld.new('Daisy').hello('Bob')
-      puts result
+        
       assert_equal 'Hello, Bob. My name is Daisy!', HelloWorld.new('Daisy').hello('Bob')
     end
   end
